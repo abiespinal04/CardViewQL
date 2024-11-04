9f23d7db5131524d16c53195fbe648b8f1cc5cb4
abiespinal04 <abimaelespinal@hotmail.com>
2024-11-03 22:06:40 -0500

    Add offline mode

--- App.tsx
+++ App.tsx
@@ -3,22 +3,44 @@ import {RelayEnvironmentProvider} from 'react-relay';
 import RelayEnvironment from './src/relay/RelayEnvironment';
 import {NavigationContainer} from '@react-navigation/native';
 import TabStackNavigator from './src/navigation/stacks/TabStackNavigator';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {NetworkConsumer, NetworkProvider} from 'react-native-offline';
 
 export default function App() {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
-      <SafeAreaView style={styles.container}>
-        <NavigationContainer>
-          <TabStackNavigator />
-        </NavigationContainer>
-      </SafeAreaView>
+      <NetworkProvider>
+        <NetworkConsumer>
+          {({isConnected}) => (
+            <SafeAreaView style={styles.container}>
+              {!isConnected && (
+                <View style={styles.offlineContainer}>
+                  <Text style={styles.offlineText}>
+                    You are currently offline
+                  </Text>
+                </View>
+              )}
+              <NavigationContainer>
+                <TabStackNavigator />
+              </NavigationContainer>
+            </SafeAreaView>
+          )}
+        </NetworkConsumer>
+      </NetworkProvider>
     </RelayEnvironmentProvider>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  offlineContainer: {
+    backgroundColor: 'red',
+    padding: 10,
+    alignItems: 'center',
+  },
+  offlineText: {
+    color: 'white',
   },
 });
